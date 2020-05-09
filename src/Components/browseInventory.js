ab02ab5632b9f5f9a518a9e6a3129d2a128b87a0
amanuel <achall1@students.towson.edu>
2020-05-09 18:49:53 -0400

    frontend hosted files

---
+++ src/Components/browseInventory.js
@@ -0,0 +1,45 @@
+import  axios  from 'axios';
+import React, { useState, useEffect } from 'react';
+import '../Styles/editAccount_style.css'
+import Footer from './footer'
+import { getVehicles } from './fetchvehicles';
+import VehicleView from './vehicleView';
+
+
+const BrowseInventory = (props) => {
+
+    const [allVehicles,setallVehicles] = useState ([]);
+    const [error, setError] = useState(false);
+    const [refresh, setRefresh] = useState(false)
+
+    const loadallVehicles = () => {
+        getVehicles().then(data => {
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setallVehicles(data);
+            }
+        });
+    };
+
+    useEffect(() => {
+        loadallVehicles();
+
+    }, []);
+    return ( 
+        <div>
+            <h2> New Arrivals cars </h2> 
+                <div>  
+                {    //vehicleView
+                allVehicles.map((product, i) => (
+                <div key={i} style={{display: 'inline-block'}} className="col-4 mb-3">
+                <VehicleView product={product} />
+                 </div>
+                 ))}
+                </div>
+                <Footer />
+        </div>
+    );
+};
+
+export default BrowseInventory;
